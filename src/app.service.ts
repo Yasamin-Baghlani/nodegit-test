25bc899cb08a60511af1f9b4224c8a77cb8cb05a
yasaminbaghlani <ybaghlani@venus.bayern>
2024-10-18 10:22:37 +0200

    Revert "changed getHello-message"

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Hello Yasamin! Hello Git';
+    return 'Hello World!';
   }
 }
