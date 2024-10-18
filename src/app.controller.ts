5f8da4af54e12efabfd87ef3de78787416a0b8aa
yasaminbaghlani <ybaghlani@venus.bayern>
2024-10-18 10:38:26 +0200

    added TestMessage instead  getHello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  /* getHello(): string {
     return this.appService.getHello();
+  }*/
+  getTestMessage(): string {
+    return this.appService.getTestMessage();
   }
 }
